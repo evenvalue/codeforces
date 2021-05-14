16a488b66f02320921bb7b393cb400437a34f13a
Naveen Kulkarni <naveenpkulkarni07@gmail.com>
2021-05-15 01:41:40 +0530

    Add interactive problem.
    Is it rated - 2 : https://codeforces.com/problemset/problem/1505/A

---
+++ 1505A.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  string s;
+  while (getline(cin, s)) {
+    cout << "NO" << endl;
+  }
+}
