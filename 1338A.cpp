4ded1a42b5af5257289f7872696e9bfe48d5533f
Naveen Kulkarni <naveenpkulkarni07@gmail.com>
2021-06-23 15:54:55 +0530

    Add bits + greedy problem.
    Powered Addition : https://codeforces.com/problemset/problem/1338/A

---
+++ 1338A.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#define debug(x) cerr << #x << ": " << x << '\n'
+
+int bit_count(int x) {
+  int cnt = 0;
+  while (x) {
+    cnt++;
+    x /= 2;
+  }
+  return cnt;
+}
+
+void solve() {
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  for (int &x : a) {
+    cin >> x;
+  }
+  int ans = 0;
+  for (int i = 1; i < n; i++) {
+    if (a[i] >= a[i - 1]) continue;
+    ans = max(ans, bit_count(a[i] - a[i - 1]));
+    a[i] = a[i - 1];
+  }
+  cout << ans << '\n';
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+}
