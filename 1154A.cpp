bcf197194dff8275e618ff23cf38256dca60033e
Naveen Kulkarni <naveenpkulkarni07@gmail.com>
2021-06-27 23:35:14 +0530

    Add math problem.
    Restoring Three Numbers : https://codeforces.com/contest/1154/problem/A

---
+++ 1154A.cpp
@@ -0,0 +1,35 @@
+#include <algorithm>
+#include <array>
+#include <iostream>
+#include <numeric>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define int64 int64_t
+#define debug(x) cerr << #x << ": " << x << '\n'
+
+const int kInf = 1e9 + 10;
+const int kMod = 1e9 + 7;
+
+void solve() {
+  array<int, 4> a{};
+  for (int &x : a) cin >> x;
+  sort(a.begin(), a.end());
+  for (int i = 0; i < 3; i++) {
+    cout << a[3] - a[i] << ' ';
+  }
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int t = 1;
+  //cin >> t;
+  while (t--) {
+    solve();
+    cout << '\n';
+  }
+}
