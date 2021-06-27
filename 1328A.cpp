21391445d524ce7016e7daddfa28afd483efc72f
Naveen Kulkarni <naveenpkulkarni07@gmail.com>
2021-06-27 23:18:32 +0530

    Add implementation problem.
    Divisibility Problem : https://codeforces.com/contest/1328/problem/A

---
+++ 1328A.cpp
@@ -0,0 +1,33 @@
+#include <algorithm>
+#include <array>
+#include <iostream>
+#include <numeric>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define int64 int64_t
+#define debug(x) cerr << #x << ": " << x << '\n'
+
+const int kInf = 1e9 + 10;
+const int kMod = 1e9 + 7;
+
+void solve() {
+  int a, b;
+  cin >> a >> b;
+  a = a % b;
+  cout << (a == 0 ? a : b - a);
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int t = 1;
+  cin >> t;
+  while (t--) {
+    solve();
+    cout << '\n';
+  }
+}
