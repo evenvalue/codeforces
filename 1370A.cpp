e03e06f17a414705390622e19deab0fa87d163f8
Naveen Kulkarni <naveenpkulkarni07@gmail.com>
2021-06-27 23:12:50 +0530

    Add implementation problem.
    Maximum GCD : https://codeforces.com/contest/1370/problem/A

---
+++ 1370A.cpp
@@ -0,0 +1,32 @@
+#include <algorithm>
+#include <array>
+#include <iostream>
+#include <numeric>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define int64 int64_t
+#define debug(x) cerr << #x << ": " << x << '\n'
+
+const int kInf = 1e9 + 10;
+const int kMod = 1e9 + 7;
+
+void solve() {
+  int n;
+  cin >> n;
+  cout << n / 2;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int t = 1;
+  cin >> t;
+  while (t--) {
+    solve();
+    cout << '\n';
+  }
+}
