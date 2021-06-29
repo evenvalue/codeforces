69f9a1d395fdb4f6ca684852b7e905c870ea14cb
Naveen Kulkarni <naveenpkulkarni07@gmail.com>
2021-06-29 13:22:45 +0530

    Add dijkstra problem.
    Dijkstra? : https://codeforces.com/problemset/problem/20/C

---
+++ 20C.cpp
@@ -0,0 +1,74 @@
+#include <algorithm>
+#include <array>
+#include <iostream>
+#include <numeric>
+#include <set>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define int64 int64_t
+#define debug(x) cerr << #x << ": " << x << '\n'
+
+const int64 kInf = 1e15 + 10;
+const int kMod = 1e9 + 7;
+
+pair<vector<int64>, vector<int>> dijkstra(const vector<vector<pair<int, int>>> &g, const int s) {
+  const int n = g.size();
+  vector<int64> d(n, kInf);
+  vector<int> p(n, -1);
+  d[s] = 0;
+  set<pair<int64, int>> q;
+  q.insert({0, s});
+  while (not q.empty()) {
+    const int x = q.begin()->second;
+    q.erase(q.begin());
+    for (const auto [y, w] : g[x]) {
+      if (d[y] < d[x] + w) continue;
+      q.erase({d[y], y});
+      d[y] = d[x] + w;
+      q.insert({d[y], y});
+      p[y] = x;
+    }
+  }
+  return make_pair(d, p);
+}
+
+void solve() {
+  int n, m;
+  cin >> n >> m;
+  vector<vector<pair<int, int>>> g(n);
+  for (int i = 0; i < m; i++) {
+    int x, y, w;
+    cin >> x >> y >> w;
+    x--, y--;
+    g[x].push_back({y, w});
+    g[y].push_back({x, w});
+  }
+  const auto [_, p] = dijkstra(g, 0);
+  if (p[n - 1] == -1) {
+    cout << -1;
+    return;
+  }
+  vector<int> path;
+  for (int x = n - 1; x != -1; x = p[x]) {
+    path.push_back(x);
+  }
+  reverse(path.begin(), path.end());
+  for (const int x : path) {
+    cout << x + 1 << ' ';
+  }
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int t = 1;
+  //cin >> t;
+  while (t--) {
+    solve();
+    cout << '\n';
+  }
+}
