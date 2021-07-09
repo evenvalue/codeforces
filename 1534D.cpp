2be5887e152b56ededa187affc4a69c79215ded6
Naveen Kulkarni <naveenpkulkarni07@gmail.com>
2021-07-09 22:13:37 +0530

    Replace adjacency matrix with a vector of edges.
    Also, replace #define int64 int64_t with using int64 = int64_t
    
    Lost Tree : https://codeforces.com/contest/1534/problem/D

--- 1534D.cpp
+++ 1534D.cpp
@@ -7,8 +7,8 @@
 #include <vector>
 using namespace std;
 
-#define int64 int64_t
 #define debug(x) cerr << #x << ": " << x << '\n'
+using int64 = int64_t;
 
 const int64 kInfL = 1e15 + 10;
 const int kInf = 1e9 + 10;
@@ -23,15 +23,12 @@ vector<int> ask(const int n, const int x) {
   return d;
 }
 
-void say(const vector<vector<int>> &g) {
-  const int n = g.size();
-  cout << '!' << endl;
-  for (int x = 0; x < n; x++) {
-    for (int y = x + 1; y < n; y++) {
-      if (!g[x][y]) continue;
-      cout << x + 1 << ' ' << y + 1 << endl;
-    }
+void say(const vector<pair<int, int>> &edges) {
+  cout << '!' << '\n';
+  for (const auto [x, y] : edges) {
+    cout << x + 1 << ' ' << y + 1 << '\n';
   }
+  cout << flush;
 }
 
 void solve() {
@@ -39,24 +36,25 @@ void solve() {
   cin >> n;
   vector<int> d = ask(n, 0);
   vector<int> red, blue;
-  vector<vector<int>> g(n, vector<int>(n));
+  vector<pair<int, int>> edges;
+  edges.reserve(n - 1);
   for (int x = 0; x < n; x++) {
     if (d[x] & 1) red.push_back(x);
     else blue.push_back(x);
     if (d[x] == 1) {
-      g[0][x] = g[x][0] = 1;
+      edges.emplace_back(0, x);
     }
   }
   if (red.size() < blue.size() - 1) swap(red, blue);
   for (const int x : blue) {
     if (x == 0) continue;
     d = ask(n, x);
-    for (int y = 0; y < n; y++) {
+    for (int y = 1; y < n; y++) {
       if (d[y] != 1) continue;
-      g[x][y] = g[y][x] = 1;
+      edges.emplace_back(x, y);
     }
   }
-  say(g);
+  say(edges);
 }
 
 int main() {
