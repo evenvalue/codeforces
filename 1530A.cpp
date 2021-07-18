52affd69bf211564babcf8e14d04405895392db4
Naveen Kulkarni <naveenpkulkarni07@gmail.com>
2021-07-18 11:32:14 +0530

    Add implementation problem.
    Binary Decimal: https://codeforces.com/contest/1530/problem/A

---
+++ 1530A.cpp
@@ -0,0 +1,38 @@
+#include <algorithm>
+#include <array>
+#include <functional>
+#include <iostream>
+#include <numeric>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define debug(x) cerr << #x << ": " << x << '\n'
+using int64 = int64_t;
+
+const int64 kInfL = 1e15 + 10;
+const int kInf = 1e9 + 10;
+const int kMod = 1e9 + 7;
+
+void solve() {
+  string s;
+  cin >> s;
+  int ans = 0;
+  for (const char c : s) {
+    ans = max(ans, c - '0');
+  }
+  cout << ans;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int t = 1;
+  cin >> t;
+  while (t--) {
+    solve();
+    cout << '\n';
+  }
+}
