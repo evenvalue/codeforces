d090d2df792ae80cacb91eddb2e2af04c22d504a
Naveen Kulkarni <naveenpkulkarni07@gmail.com>
2021-07-20 21:30:31 +0530

    Add greedy + math problem.
    Customising the Track: https://codeforces.com/contest/1543/problem/B

---
+++ 1543B.cpp
@@ -0,0 +1,37 @@
+#include <algorithm>
+#include <array>
+#include <cmath>
+#include <functional>
+#include <iostream>
+#include <numeric>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define debug(x) cerr << #x << ": " << x << '\n'
+using int64 = int64_t;
+
+const int64 kInfL = 1e15 + 10;
+const int kInf = 1e9 + 10;
+const int kMod = 1e9 + 7;
+
+void solve() {
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  for (int &x : a) cin >> x;
+  const int64 extra = (accumulate(a.begin(), a.end(), 0LL)) % n;
+  cout << extra * (n - extra) << '\n';
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int t = 1;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+}
