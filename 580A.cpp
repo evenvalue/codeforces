9d85b45a0e2810d89e70df0a631038074ca0ed16
Naveen Kulkarni <naveenpkulkarni07@gmail.com>
2021-07-24 22:03:54 +0530

    Add implementation problem.
    Kefa and First Steps: https://codeforces.com/problemset/problem/580/A

---
+++ 580A.cpp
@@ -0,0 +1,41 @@
+#include <algorithm>
+#include <array>
+#include <cmath>
+#include <functional>
+#include <iostream>
+#include <numeric>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define debug(x) cerr << #x << ": " << x << '\n'
+using int64 = int64_t;
+
+const int64 kInfL = 1e15 + 10;
+const int kInf = 1e9 + 10;
+const int kMod = 1e9 + 7;
+
+void solve() {
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  for (int &x : a) cin >> x;
+  int cnt = 1, ans = 1;
+  for (int i = 1; i < n; i++) {
+    cnt = (a[i - 1] <= a[i] ? cnt + 1 : 1);
+    ans = max(ans, cnt);
+  }
+  cout << ans << '\n';
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int t = 1;
+  //cin >> t;
+  while (t--) {
+    solve();
+  }
+}
