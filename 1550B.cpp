95fed76dea342b8b5e97240d6e333e1e5f799b7b
Naveen Kulkarni <naveenpkulkarni07@gmail.com>
2021-07-24 23:05:24 +0530

    Add math problem.
    Maximum Cost Deletion: https://codeforces.com/contest/1550/problem/B

---
+++ 1550B.cpp
@@ -0,0 +1,36 @@
+#include <algorithm>
+#include <array>
+#include <cmath>
+#include <functional>
+#include <iostream>
+#include <numeric>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define debug(x) cerr << #x << ": " << x << '\n'
+using int64 = int64_t;
+
+const int64 kInfL = 1e15 + 10;
+const int kInf = 1e9 + 10;
+const int kMod = 1e9 + 7;
+
+void solve() {
+  int n, a, b;
+  string s;
+  cin >> n >> a >> b >> s;
+  const int k = (b >= 0 ? n : (int)(distance(s.begin(), unique(s.begin(), s.end()))) / 2 + 1);
+  cout << a * n + k * b << '\n';
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int t = 1;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+}
