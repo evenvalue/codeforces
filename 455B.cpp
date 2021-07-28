11f62b179ca47240e01d47ca9a0df23ed75a7b7b
Naveen Kulkarni <naveenpkulkarni07@gmail.com>
2021-07-28 22:45:28 +0530

    Make a class for trie.

--- 455B.cpp
+++ 455B.cpp
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <array>
+#include <cassert>
 #include <cmath>
 #include <functional>
 #include <iostream>
@@ -15,29 +16,72 @@ const int64 kInfL = 1e15 + 10;
 const int kInf = 1e9 + 10;
 const int kMod = 1e9 + 7;
 
-void solve() {
+namespace evenvalue {
+class trie {
+  vector<vector<int>> t;
+  vector<bool> terminal;
+  int free;
+
+public:
+  explicit trie() {
+    t.emplace_back(26, -1);
+    terminal.push_back(false);
+    free = 1;
+  }
+
+  // Adds string s to the trie.
+  // Time Complexity: O(s.length()).
+  void add(const string &s) {
+    int x = 0;
+    for (const char c : s) {
+      if (t[x][c - 'a'] == -1) {
+        t.emplace_back(26, -1);
+        terminal.push_back(false);
+        t[x][c - 'a'] = free++;
+      }
+      x = t[x][c - 'a'];
+    }
+    terminal[x] = true;
+  }
+
+  //Returns the number of nodes in the trie.
+  //Time Complexity: O(1).
+  [[nodiscard]] int nodes() const {
+    return free;
+  }
+
+  //Returns the node index of x.secondTh child of x.first, if any, else returns -1.
+  //Time Complexity: O(1).
+  int operator[](const pair<int, int> &x) const {
+    assert(0 <= x.first and x.first < free);
+    assert(0 <= x.second and x.second <= t[x.first].size());
+    return t[x.first][x.second];
+  }
+
+  //Returns whether node x is a terminal node or not.
+  //Time Complexity: O(1).
+  bool operator[](const int x) const {
+    assert(0 <= x and x < free);
+    return terminal[x];
+  }
+};
+}// namespace evenvalue
+
+bool solve() {
+  evenvalue::trie t;
   int n, k;
   cin >> n >> k;
-  vector<vector<int>> trie(1, vector<int>(26, -1));
-  int free = 1;
   for (int i = 0; i < n; i++) {
     string s;
     cin >> s;
-    int x = 0;
-    for (const char c : s) {
-      if (trie[x][c - 'a'] == -1) {
-        trie[x][c - 'a'] = free++;
-        trie.emplace_back(26, -1);
-      }
-      x = trie[x][c - 'a'];
-    }
+    t.add(s);
   }
-  vector<bool> win(free, false);
-  vector<bool> lose(free, false);
+  vector<bool> win(t.nodes(), false);
+  vector<bool> lose(t.nodes(), false);
   function<void(int)> dfs = [&](const int x) {
     bool leaf = true;
     for (int c = 0; c < 26; c++) {
-      if (const int y = trie[x][c]; y != -1) {
+      if (const int y = t[{x, c}]; y != -1) {
         dfs(y);
         win[x] = win[x] or not win[y];
         lose[x] = lose[x] or not lose[y];
@@ -48,13 +92,12 @@ void solve() {
   };
   dfs(0);
   if (not win[0]) {
-    cout << "Second";
-  } else if (win[0] and lose[0]) {
-    cout << "First";
+    return false;
+  } else if (lose[0]) {
+    return true;
   } else {
-    cout << (k & 1 ? "First" : "Second");
+    return (k & 1);
   }
-  cout << '\n';
 }
 
 int main() {
@@ -65,6 +108,6 @@ int main() {
   int t = 1;
   //cin >> t;
   while (t--) {
-    solve();
+    cout << (solve() ? "First" : "Second") << '\n';
   }
 }
