de2277ab91032082d25353946028673cb8fd4860
Naveen Kulkarni <naveenpkulkarni07@gmail.com>
2021-07-30 12:34:41 +0530

    Add geometry problem.
    Data Center: https://codeforces.com/contest/1250/problem/F

---
+++ 1250F.cpp
@@ -0,0 +1,40 @@
+#include <algorithm>
+#include <array>
+#include <cmath>
+#include <functional>
+#include <iostream>
+#include <numeric>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define debug(x) cerr << #x << ": " << x << '\n'
+using int64 = int64_t;
+
+const int64 kInfL = 1e15 + 10;
+const int kInf = 1e9 + 10;
+const int kMod = 1e9 + 7;
+
+void solve() {
+  int n;
+  cin >> n;
+  int ans = kInf;
+  for (int h = 1; h <= n; h++) {
+    if (n % h) continue;
+    const int w = n / h;
+    ans = min(ans, 2 * (h + w));
+  }
+  cout << ans << '\n';
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int t = 1;
+  //cin >> t;
+  while (t--) {
+    solve();
+  }
+}
