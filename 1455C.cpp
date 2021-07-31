1f7ddc65f42dfde17bb6933da72d885df5594ec7
Naveen Kulkarni <naveenpkulkarni07@gmail.com>
2021-07-31 10:25:33 +0530

    Add game theory problem.
    Ping-pong: https://codeforces.com/problemset/problem/1455/C

---
+++ 1455C.cpp
@@ -0,0 +1,34 @@
+#include <algorithm>
+#include <array>
+#include <cmath>
+#include <functional>
+#include <iostream>
+#include <numeric>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define debug(x) cerr << #x << ": " << x << '\n'
+using int64 = int64_t;
+
+const int64 kInfL = 1e15 + 10;
+const int kInf = 1e9 + 10;
+const int kMod = 1e9 + 7;
+
+void solve() {
+  int x, y;
+  cin >> x >> y;
+  cout << x - 1 << ' ' << y << '\n';
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int t = 1;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+}
