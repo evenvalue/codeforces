2916ecba7ce7a7eb1cd5ed4cd2650f3dd2d2eb51
Naveen Kulkarni <naveenpkulkarni07@gmail.com>
2021-08-02 21:45:04 +0530

    Add math problem.
    PizzaForces: https://codeforces.com/contest/1555/problem/A

---
+++ 1555A.cpp
@@ -0,0 +1,36 @@
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <cmath>
+#include <functional>
+#include <iostream>
+#include <numeric>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define debug(x) cerr << #x << ": " << x << '\n'
+using int64 = int64_t;
+
+const int64 kInfL = 1e15 + 10;
+const int kInf = 1e9 + 10;
+const int kMod = 1e9 + 7;
+
+void solve() {
+  int64 n;
+  cin >> n;
+  n = max(n, 6LL);
+  cout << 5 * ((n + 1) / 2) << '\n';
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int t = 1;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+}
