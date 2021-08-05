5ab697b8c1b6073e73a7390526eb6cb57eb5c74f
Naveen Kulkarni <naveenpkulkarni07@gmail.com>
2021-08-05 20:37:48 +0530

    Add strings problem.
    Way Too Long Words: https://codeforces.com/problemset/problem/71/A

---
+++ 71A.cpp
@@ -0,0 +1,40 @@
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <cmath>
+#include <functional>
+#include <iostream>
+#include <numeric>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define debug(x) cerr << #x << ": " << x << '\n'
+using int64 = int64_t;
+
+const int64 kInfL = 1e15 + 10;
+const int kInf = 1e9 + 10;
+const int kMod = 1e9 + 7;
+
+void solve() {
+  string s;
+  cin >> s;
+  if (s.length() <= 10) {
+    cout << s;
+  } else {
+    cout << s[0] << s.length() - 2 << s.back();
+  }
+  cout << '\n';
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int t = 1;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+}
