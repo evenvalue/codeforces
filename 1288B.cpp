ebff14bd6343ebcc8eae71092638ff4bd1a86916
Naveen Kulkarni <naveenpkulkarni07@gmail.com>
2021-08-12 15:05:40 +0530

    Add math problem.
    Yet Another Meme Problem: https://codeforces.com/problemset/problem/1288/B

---
+++ 1288B.cpp
@@ -0,0 +1,35 @@
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <cmath>
+#include <functional>
+#include <iostream>
+#include <numeric>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define debug(x) cerr << #x << ": " << x << '\n'
+using int64 = int64_t;
+
+const int64 kInfL = 1e15 + 10;
+const int kInf = 1e9 + 10;
+const int kMod = 1e9 + 7;
+
+void solve() {
+  int a, b;
+  cin >> a >> b;
+  cout << (int64)a * (to_string(b + 1).length() - 1) << '\n';1
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int t = 1;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+}
