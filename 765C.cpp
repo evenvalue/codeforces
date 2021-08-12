e3f6bbee0f688ec5c719100b429f54cdf1f2c31c
Naveen Kulkarni <naveenpkulkarni07@gmail.com>
2021-08-12 21:10:38 +0530

    Add math problem.
    Table Tennis Game 2: https://codeforces.com/problemset/problem/765/C

---
+++ 765C.cpp
@@ -0,0 +1,36 @@
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <cmath>
+#include <functional>
+#include <iostream>
+#include <numeric>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define debug(x) cerr << #x << ": " << x << '\n'
+using int64 = int64_t;
+
+const int64 kInfL = 1e15 + 10;
+const int kInf = 1e9 + 10;
+const int kMod = 1e9 + 7;
+
+void solve() {
+  int k, a, b;
+  cin >> k >> a >> b;
+  if (a < b) swap(a, b);
+  cout << ((b >= k or a % k == 0) ? a / k + b / k : -1) << '\n';
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int t = 1;
+  //cin >> t;
+  while (t--) {
+    solve();
+  }
+}
