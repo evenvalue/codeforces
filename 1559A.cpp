d57cc724936721e92f0397b672307744d7a24beb
Naveen Kulkarni <naveenpkulkarni07@gmail.com>
2021-08-16 23:36:38 +0530

    Add bits problem.
    Mocha and Math: https://codeforces.com/problemset/problem/1559/A

---
+++ 1559A.cpp
@@ -0,0 +1,40 @@
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <cmath>
+#include <functional>
+#include <iostream>
+#include <numeric>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define debug(x) cerr << #x << ": " << x << '\n'
+using int64 = int64_t;
+
+const int64 kInfL = 1e15 + 10;
+const int kInf = 1e9 + 10;
+const int kMod = 1e9 + 7;
+
+void solve() {
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  for (int &x : a) cin >> x;
+  int ans = a[0];
+  for (const int x : a) ans &= x;
+  cout << ans << '\n';
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int t = 1;
+
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+}
