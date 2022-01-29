35786a38dbac994566f1a0170a49f826fd07c6d4
evenvalue <naveenpkulkarni07@gmail.com>
2022-01-29 23:56:10 +0530

    Add dp problem.
    Consecutive Subsequence: https://codeforces.com/contest/977/problem/F

---
+++ 977F.cpp
@@ -0,0 +1,123 @@
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <cstdio>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <string>
+#include <vector>
+using namespace std;
+
+using int64 = int64_t;
+using ld = long double;
+
+template<typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+template<typename T>
+using max_heap = priority_queue<T, vector<T>, less<T>>;
+
+[[maybe_unused]] int readInt() {
+  int x;
+  cin >> x;
+  return x;
+}
+[[maybe_unused]] int64 readInt64() {
+  int64 x;
+  cin >> x;
+  return x;
+}
+[[maybe_unused]] char readChar() {
+  char c;
+  cin >> c;
+  return c;
+}
+[[maybe_unused]] string readString() {
+  string s;
+  cin >> s;
+  return s;
+}
+[[maybe_unused]] double readDouble() {
+  return stod(readString());
+}
+[[maybe_unused]] ld readLongDouble() {
+  return stold(readString());
+}
+template<typename T1, typename T2>
+[[maybe_unused]] pair<T1, T2> readPair() {
+  pair<T1, T2> p;
+  cin >> p.first >> p.second;
+  return p;
+}
+template<typename T>
+[[maybe_unused]] vector<T> readVec(const int sz) {
+  vector<T> v(sz);
+  for (T &x : v) {
+    cin >> x;
+  }
+  return v;
+}
+template<typename T>
+[[maybe_unused]] vector<vector<T>> readVecVec(const int n, const int m) {
+  vector<vector<T>> a(n);
+  for (vector<T> &v : a) {
+    v = readVec<T>(m);
+  }
+  return a;
+}
+
+const int64 kInf64 = 2e18 + 10;
+const int kInf = 1e9 + 10;
+const int kMod = 1e9 + 7;
+
+void solution() {
+  const int n = readInt();
+  const vector<int> &a = readVec<int>(n);
+  map<int, int> dp;
+  map<int, vector<int>> occ;
+  for (int i = 0; i < n; i++) {
+    dp[a[i]] = max(dp[a[i]], dp[a[i] - 1] + 1);
+    occ[a[i]].push_back(i);
+  }
+  int last = -1, k = -1;
+  for (const auto &[i, l] : dp) {
+    if (k < l) {
+      last = i, k = l;
+    }
+  }
+  cout << k << '\n';
+  int first = last - k + 1;
+  for (int i = first, idx = -1; i <= last; i++) {
+    const auto &v = occ[i];
+    idx = *upper_bound(v.begin(), v.end(), idx);
+    cout << idx + 1 << ' ';
+  }
+  cout << '\n';
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  const auto begin = std::chrono::high_resolution_clock::now();
+
+//  freopen(".in", "r", stdin);
+//  freopen(".out", "w", stdout);
+
+  int t = 1;
+  //cin >> t;
+  for (int tc = 1; tc <= t; tc++) {
+    solution();
+  }
+
+  const auto end = std::chrono::high_resolution_clock::now();
+  const auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+  cerr << "Time measured: " << elapsed.count() * 1e-9 << " seconds.\n";
+}
