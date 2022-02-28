6d6cd2ea3aca827de029627328dcd541ead1d914
evenvalue <naveenpkulkarni07@gmail.com>
2022-02-28 13:33:51 +0530

    Add priority queue problem.
    Captain Flint and Treasure: https://codeforces.com/problemset/problem/1388/D

---
+++ 1388D.cpp
@@ -0,0 +1,140 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+#ifdef evenvalue
+  #include "debug.h"
+  #define debug(...) print(#__VA_ARGS__, __VA_ARGS__)
+#else
+  #define debug(...)
+#endif
+
+using int64 = int64_t;
+using ld = long double;
+
+template<typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<typename T>
+using ordered_multiset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+template<typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+template<typename T>
+using max_heap = priority_queue<T, vector<T>, less<T>>;
+
+namespace read {
+int Int() {
+  int x;
+  cin >> x;
+  return x;
+}
+int64 Int64() {
+  int64 x;
+  cin >> x;
+  return x;
+}
+char Char() {
+  char c;
+  cin >> c;
+  return c;
+}
+string String() {
+  string s;
+  cin >> s;
+  return s;
+}
+double Double() {
+  return stod(String());
+}
+ld LongDouble() {
+  return stold(String());
+}
+template<typename T1, typename T2>
+pair<T1, T2> Pair() {
+  pair<T1, T2> p;
+  cin >> p.first >> p.second;
+  return p;
+}
+template<typename T>
+vector<T> Vec(const int n) {
+  vector<T> v(n);
+  for (T &x : v) {
+    cin >> x;
+  }
+  return v;
+}
+template<typename T>
+vector<vector<T>> VecVec(const int n, const int m) {
+  vector<vector<T>> v(n);
+  for (vector<T> &vec : v) {
+    vec = Vec<T>(m);
+  }
+  return v;
+}
+}//namespace read
+
+constexpr int kInf = 1e9 + 10;
+constexpr int64 kInf64 = 1e15 + 10;
+constexpr int kMod = 1e9 + 7;
+
+void solution() {
+  const int n = read::Int();
+  vector<int64> a = read::Vec<int64>(n);
+  vector<int> p = read::Vec<int>(n), children(n);
+
+  for (int x = 0; x < n; x++) {
+    if (p[x] == -1) continue;
+    p[x]--;
+    children[p[x]]++;
+  }
+
+  max_heap<pair<int64, int>> heap;
+
+  for (int x = 0; x < n; x++)
+    if (children[x] == 0)
+      heap.push({a[x], x});
+
+  int64 ans = 0;
+  vector<int> first, last;
+
+  while (not heap.empty()) {
+    const auto [val, x] = heap.top();
+    ans += val;
+    heap.pop();
+    if (val <= 0)
+      last.push_back(x + 1);
+    else
+      first.push_back(x + 1);
+    if (p[x] != -1) {
+      a[p[x]] += max(0LL, val);
+      if (--children[p[x]] == 0)
+        heap.push({a[p[x]], p[x]});
+    }
+  }
+
+  cout << ans << '\n';
+  reverse(last.begin(), last.end());
+  for (const int x : first)
+    cout << x << ' ';
+  for (const int x : last)
+    cout << x << ' ';
+  cout << '\n';
+}
+
+int32_t main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  // freopen(".in", "r", stdin);
+  // freopen(".out", "w", stdout);
+
+  cout << fixed << setprecision(10);
+
+  int testcases = 1;
+  //cin >> testcases;
+  while (testcases--) {
+    solution();
+  }
+}
