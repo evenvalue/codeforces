0e10b93ab0bacc6595072c947ab03124dc1c428f
evenvalue <naveenpkulkarni07@gmail.com>
2022-03-05 23:49:46 +0530

    Add Square Root optimisation problem.
    Array Queries: https://codeforces.com/contest/797/problem/E

---
+++ 797E.cpp
@@ -0,0 +1,133 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+#ifdef evenvalue
+  #include "debug.h"
+  #define debug(...) print(#__VA_ARGS__, __VA_ARGS__)
+#else
+  #define debug(...)
+#endif
+
+using int64 = int64_t;
+using ld = long double;
+
+template<typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<typename T>
+using ordered_multiset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+template<typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+template<typename T>
+using max_heap = priority_queue<T, vector<T>, less<T>>;
+
+namespace read {
+int Int() {
+  int x;
+  cin >> x;
+  return x;
+}
+int64 Int64() {
+  int64 x;
+  cin >> x;
+  return x;
+}
+char Char() {
+  char c;
+  cin >> c;
+  return c;
+}
+string String() {
+  string s;
+  cin >> s;
+  return s;
+}
+double Double() {
+  return stod(String());
+}
+ld LongDouble() {
+  return stold(String());
+}
+template<typename T1, typename T2>
+pair<T1, T2> Pair() {
+  pair<T1, T2> p;
+  cin >> p.first >> p.second;
+  return p;
+}
+template<typename T>
+vector<T> Vec(const int n) {
+  vector<T> v(n);
+  for (T &x : v) {
+    cin >> x;
+  }
+  return v;
+}
+template<typename T>
+vector<vector<T>> VecVec(const int n, const int m) {
+  vector<vector<T>> v(n);
+  for (vector<T> &vec : v) {
+    vec = Vec<T>(m);
+  }
+  return v;
+}
+}//namespace read
+
+constexpr int kInf = 1e9 + 10;
+constexpr int64 kInf64 = 1e15 + 10;
+constexpr int kMod = 1e9 + 7;
+constexpr int kSqrtN = 500;
+
+void solution() {
+  const int n = read::Int();
+  const vector<int> a = read::Vec<int>(n);
+
+  vector<vector<int>> precalc(kSqrtN + 1, vector<int>(n));
+
+  for (int k = 1; k <= kSqrtN; k++) {
+    for (int i = n - 1; i >= 0; i--) {
+      const int nxt = i + a[i] + k;
+      precalc[k][i] = (nxt >= n ? 0 : precalc[k][nxt]) + 1;
+    }
+  }
+
+  auto kIsLessThanSqrtN = [&](const int p, const int k) {
+    return precalc[k][p];
+  };
+
+  auto kIsGreaterThanSqrtN = [&](int p, const int k) {
+    int steps = 0;
+    for (; p < n; p += a[p] + k) {
+      steps++;
+    }
+    return steps;
+  };
+
+  int q = read::Int();
+  while (q--) {
+    const int p = read::Int() - 1, k = read::Int();
+    if (k <= kSqrtN) {
+      cout << kIsLessThanSqrtN(p, k) << '\n';
+    } else {
+      cout << kIsGreaterThanSqrtN(p, k) << '\n';
+    }
+  }
+}
+
+int32_t main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  // freopen(".in", "r", stdin);
+  // freopen(".out", "w", stdout);
+
+  cout << fixed << setprecision(10);
+
+  int testcases = 1;
+  //cin >> testcases;
+  while (testcases--) {
+    solution();
+  }
+}
