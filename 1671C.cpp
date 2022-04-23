3e6c5b9e1411ec0399cecd17201e57ed1a8ad411
evenvalue <naveenpkulkarni07@gmail.com>
2022-04-23 20:00:57 +0530

    Add solutions to Educational Codeforces Round 127
    Dashboard: https://codeforces.com/contest/1671

---
+++ 1671C.cpp
@@ -0,0 +1,125 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+#ifdef evenvalue
+  #include "debug.h"
+  #define debug(...) print(#__VA_ARGS__, __VA_ARGS__)
+#else
+  #define debug(...)
+#endif
+
+using int64 = int64_t;
+using ld = long double;
+
+template<typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<typename T>
+using ordered_multiset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+template<typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+template<typename T>
+using max_heap = priority_queue<T, vector<T>, less<T>>;
+
+namespace read {
+int Int() {
+  int x;
+  cin >> x;
+  return x;
+}
+int64 Int64() {
+  int64 x;
+  cin >> x;
+  return x;
+}
+char Char() {
+  char c;
+  cin >> c;
+  return c;
+}
+string String() {
+  string s;
+  cin >> s;
+  return s;
+}
+double Double() {
+  return stod(String());
+}
+ld LongDouble() {
+  return stold(String());
+}
+template<typename T1, typename T2>
+pair<T1, T2> Pair() {
+  pair<T1, T2> p;
+  cin >> p.first >> p.second;
+  return p;
+}
+template<typename T>
+vector<T> Vec(const int n) {
+  vector<T> v(n);
+  for (T &x : v) {
+    cin >> x;
+  }
+  return v;
+}
+template<typename T>
+vector<vector<T>> VecVec(const int n, const int m) {
+  vector<vector<T>> v(n);
+  for (vector<T> &vec : v) {
+    vec = Vec<T>(m);
+  }
+  return v;
+}
+}//namespace read
+
+constexpr int kInf = 1e9 + 10;
+constexpr int64 kInf64 = 1e15 + 10;
+constexpr int kMod = 1e9 + 7;
+
+inline void solution() {
+  const int n = read::Int();
+  int64 x = read::Int();
+  vector<int> a = read::Vec<int>(n);
+  sort(a.begin(), a.end());
+
+  auto bs = [](const int start_ap, const int diff, const int64 t) {
+    int lo = 0, hi = 1e9 + 10;
+    while (lo < hi) {
+      const int mid = (lo + hi + 1) / 2;
+      const int64 ap_mid = start_ap + (mid - 1) * 1LL * diff;
+      if (ap_mid <= t) {
+        lo = mid;
+      } else {
+        hi = mid - 1;
+      }
+    }
+    return lo;
+  };
+
+  int64 ans = 0;
+  for (int i = 0; i < n; i++) {
+    const int inc = i + 1;
+    ans += bs(a[i], inc, x);
+    x -= a[i];
+  }
+  cout << ans << '\n';
+}
+
+int32_t main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  // freopen(".in", "r", stdin);
+  // freopen(".out", "w", stdout);
+
+  cout << fixed << setprecision(10);
+
+  int testcases = 1;
+  cin >> testcases;
+  while (testcases--) {
+    solution();
+  }
+}
