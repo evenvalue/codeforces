6d400ec80a30bca379a17a35658e855f261da4b7
Naveen Kulkarni <naveenpkulkarni07@gmail.com>
2023-12-22 19:37:20 +0530

    Add interactive problem.
    Enchanted Artifact: https://codeforces.com/contest/1282/problem/D

---
+++ 1282D.cpp
@@ -0,0 +1,120 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef evenvalue
+  #include "debug.h"
+#else
+  #define debug(...)
+#endif
+
+using int64 = long long;
+using ld = long double;
+
+template<typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+template<typename T>
+using max_heap = priority_queue<T, vector<T>, less<T>>;
+
+namespace read {
+int Int() {
+  int x;
+  cin >> x;
+  return x;
+}
+int64 Int64() {
+  int64 x;
+  cin >> x;
+  return x;
+}
+char Char() {
+  char c;
+  cin >> c;
+  return c;
+}
+string String() {
+  string s;
+  cin >> s;
+  return s;
+}
+double Double() {
+  return stod(String());
+}
+ld LongDouble() {
+  return stold(String());
+}
+template<typename T1, typename T2>
+pair<T1, T2> Pair() {
+  pair<T1, T2> p;
+  cin >> p.first >> p.second;
+  return p;
+}
+template<typename T>
+vector<T> Vec(const int n) {
+  vector<T> v(n);
+  for (T &x : v) {
+    cin >> x;
+  }
+  return v;
+}
+template<typename T>
+vector<vector<T>> VecVec(const int n, const int m) {
+  vector<vector<T>> v(n);
+  for (vector<T> &vec : v) {
+    vec = Vec<T>(m);
+  }
+  return v;
+}
+}//namespace read
+
+constexpr int kInf = 1e9 + 10;
+constexpr int64 kInf64 = 1e15 + 10;
+constexpr int kMod = 1e9 + 7;
+constexpr int kMaxN = 2e5 + 10;
+
+int ask(const string &s) {
+  cout << s << endl;
+  const int x = read::Int();
+  if (x == 0) exit(0);
+  return x;
+}
+
+const int kMaxLength = 300;
+inline void solution() {
+  const int cnt_a = kMaxLength - ask(string(kMaxLength, 'a'));
+  const int cnt_b = kMaxLength - ask(string(kMaxLength, 'b'));
+
+  if (cnt_a == 0) {
+    ask(string(cnt_b, 'b'));
+  } else if (cnt_b == 0) {
+    ask(string(cnt_a, 'a'));
+  }
+
+  const int n = cnt_a + cnt_b;
+  string s(n, 'a');
+
+  int b = cnt_b;
+  for (int i = 0; i < n; i++) {
+    s[i] = 'b';
+    if (ask(s) < b) {
+      b--;
+    } else {
+      s[i] = 'a';
+    }
+  }
+}
+
+int32_t main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  //freopen(".in", "r", stdin);
+  //freopen(".out", "w", stdout);
+
+  cout << fixed << setprecision(10);
+
+  int testcases = 1;
+  //cin >> testcases;
+  while (testcases--) {
+    solution();
+  }
+}
