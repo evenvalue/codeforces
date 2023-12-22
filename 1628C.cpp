b30db5157dc560ad3e9e4dc2f93881b7ded3b562
Naveen Kulkarni <naveenpkulkarni07@gmail.com>
2023-12-22 16:33:22 +0530

    Add greedy + constructive algorithm problem.
    Grid Xor: https://codeforces.com/contest/1628/problem/C

---
+++ 1628C.cpp
@@ -0,0 +1,115 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef evenvalue
+  #include "debug.h"
+#else
+  #define debug(...)
+#endif
+
+using int64 = long long;
+using ld = long double;
+
+template<typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+template<typename T>
+using max_heap = priority_queue<T, vector<T>, less<T>>;
+
+namespace read {
+int Int() {
+  int x;
+  cin >> x;
+  return x;
+}
+int64 Int64() {
+  int64 x;
+  cin >> x;
+  return x;
+}
+char Char() {
+  char c;
+  cin >> c;
+  return c;
+}
+string String() {
+  string s;
+  cin >> s;
+  return s;
+}
+double Double() {
+  return stod(String());
+}
+ld LongDouble() {
+  return stold(String());
+}
+template<typename T1, typename T2>
+pair<T1, T2> Pair() {
+  pair<T1, T2> p;
+  cin >> p.first >> p.second;
+  return p;
+}
+template<typename T>
+vector<T> Vec(const int n) {
+  vector<T> v(n);
+  for (T &x : v) {
+    cin >> x;
+  }
+  return v;
+}
+template<typename T>
+vector<vector<T>> VecVec(const int n, const int m) {
+  vector<vector<T>> v(n);
+  for (vector<T> &vec : v) {
+    vec = Vec<T>(m);
+  }
+  return v;
+}
+}//namespace read
+
+constexpr int kInf = 1e9 + 10;
+constexpr int64 kInf64 = 1e15 + 10;
+constexpr int kMod = 1e9 + 7;
+constexpr int kMaxN = 2e5 + 10;
+
+inline void solution() {
+  const int n = read::Int();
+  vector<vector<int>> grid = read::VecVec<int>(n, n);
+  grid.insert(grid.begin(), vector<int>(n, 0));
+
+  vector<vector<int>> parity(n + 1, vector<int>(n));
+
+  int ans = 0;
+  auto mark = [&](const int r, const int c) {
+    ans ^= grid[r][c];
+    if (c != 0) parity[r][c - 1]++;
+    if (c != n - 1) parity[r][c + 1]++;
+    if (r != n - 1) parity[r + 1][c]++;
+  };
+
+  for (int r = 1; r <= n; r++) {
+    vector<int> cur(n);
+    for (int c = 0; c < n; c++) {
+      if (parity[r - 1][c] % 2 == 0) {
+        mark(r, c);
+      }
+    }
+  }
+
+  cout << ans << '\n';
+}
+
+int32_t main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  //freopen(".in", "r", stdin);
+  //freopen(".out", "w", stdout);
+
+  cout << fixed << setprecision(10);
+
+  int testcases = 1;
+  cin >> testcases;
+  while (testcases--) {
+    solution();
+  }
+}
