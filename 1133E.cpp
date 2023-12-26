6b89eac429da68496cb6406b53fae5d8f9d79768
Naveen Kulkarni <naveenpkulkarni07@gmail.com>
2023-12-26 14:16:52 +0530

    Add dp problem.

---
+++ 1133E.cpp
@@ -0,0 +1,113 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef evenvalue
+  #include "debug.h"
+#else
+  #define debug(...)
+#endif
+
+using int64 = long long;
+using ld = long double;
+
+template<typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+template<typename T>
+using max_heap = priority_queue<T, vector<T>, less<T>>;
+
+namespace read {
+int Int() {
+  int x;
+  cin >> x;
+  return x;
+}
+int64 Int64() {
+  int64 x;
+  cin >> x;
+  return x;
+}
+char Char() {
+  char c;
+  cin >> c;
+  return c;
+}
+string String() {
+  string s;
+  cin >> s;
+  return s;
+}
+double Double() {
+  return stod(String());
+}
+ld LongDouble() {
+  return stold(String());
+}
+template<typename T1, typename T2>
+pair<T1, T2> Pair() {
+  pair<T1, T2> p;
+  cin >> p.first >> p.second;
+  return p;
+}
+template<typename T>
+vector<T> Vec(const int n) {
+  vector<T> v(n);
+  for (T &x : v) {
+    cin >> x;
+  }
+  return v;
+}
+template<typename T>
+vector<vector<T>> VecVec(const int n, const int m) {
+  vector<vector<T>> v(n);
+  for (vector<T> &vec : v) {
+    vec = Vec<T>(m);
+  }
+  return v;
+}
+}//namespace read
+
+constexpr int kInf = 1e9 + 10;
+constexpr int64 kInf64 = 1e15 + 10;
+constexpr int kMod = 1e9 + 7;
+constexpr int kMaxN = 2e5 + 10;
+
+inline void solution() {
+  const int n = read::Int();
+  const int k = read::Int();
+
+  vector<int> skill = read::Vec<int>(n);
+  sort(skill.begin(), skill.end());
+
+  vector<vector<int>> best(n, vector<int>(k + 1));
+  for (int groups = 1; groups <= k; groups++) {
+    best[0][groups] = 1;
+  }
+  for (int i = 1; i < n; i++) {
+    for (int groups = 1; groups <= k; groups++) {
+      const int j = distance(skill.begin(), lower_bound(skill.begin(), skill.end(), skill[i] - 5));
+      if (j == 0) {
+        best[i][groups] = i + 1;
+      } else {
+        best[i][groups] = max(best[i - 1][groups], (i - j + 1) + best[j - 1][groups - 1]);
+      }
+    }
+  }
+
+  cout << best[n - 1][k] << '\n';
+}
+
+int32_t main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  //freopen(".in", "r", stdin);
+  //freopen(".out", "w", stdout);
+
+  cout << fixed << setprecision(10);
+
+  int testcases = 1;
+  //cin >> testcases;
+  while (testcases--) {
+    solution();
+  }
+}
