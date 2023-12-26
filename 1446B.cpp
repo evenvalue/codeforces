348cd85811045904c293b5a43c367bea24035d6d
Naveen Kulkarni <naveenpkulkarni07@gmail.com>
2023-12-26 21:43:44 +0530

    Add dp problem.
    Catching Cheaters: https://codeforces.com/problemset/problem/1446/B

---
+++ 1446B.cpp
@@ -0,0 +1,114 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef evenvalue
+  #include "debug.h"
+#else
+  #define debug(...)
+#endif
+
+using int64 = long long;
+using ld = long double;
+
+template<typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+template<typename T>
+using max_heap = priority_queue<T, vector<T>, less<T>>;
+
+namespace read {
+int Int() {
+  int x;
+  cin >> x;
+  return x;
+}
+int64 Int64() {
+  int64 x;
+  cin >> x;
+  return x;
+}
+char Char() {
+  char c;
+  cin >> c;
+  return c;
+}
+string String() {
+  string s;
+  cin >> s;
+  return s;
+}
+double Double() {
+  return stod(String());
+}
+ld LongDouble() {
+  return stold(String());
+}
+template<typename T1, typename T2>
+pair<T1, T2> Pair() {
+  pair<T1, T2> p;
+  cin >> p.first >> p.second;
+  return p;
+}
+template<typename T>
+vector<T> Vec(const int n) {
+  vector<T> v(n);
+  for (T &x : v) {
+    cin >> x;
+  }
+  return v;
+}
+template<typename T>
+vector<vector<T>> VecVec(const int n, const int m) {
+  vector<vector<T>> v(n);
+  for (vector<T> &vec : v) {
+    vec = Vec<T>(m);
+  }
+  return v;
+}
+}//namespace read
+
+constexpr int kInf = 1e9 + 10;
+constexpr int64 kInf64 = 1e15 + 10;
+constexpr int kMod = 1e9 + 7;
+constexpr int kMaxN = 2e5 + 10;
+
+inline void solution() {
+  const int n = read::Int();
+  const int m = read::Int();
+
+  const string a = " " + read::String();
+  const string b = " " + read::String();
+
+  vector<vector<int>> dp(n + 1, vector<int>(m + 1));
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= m; j++) {
+      if (a[i] == b[j]) {
+        dp[i][j] = dp[i - 1][j - 1] + 2;
+      } else {
+        dp[i][j] = max({1, dp[i][j - 1], dp[i - 1][j]}) - 1;
+      }
+    }
+  }
+
+  int ans = 0;
+  for (int i = 1; i <= n; i++) {
+    ans = max(ans, *max_element(dp[i].begin(), dp[i].end()));
+  }
+
+  cout << ans << '\n';
+}
+
+int32_t main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  //freopen(".in", "r", stdin);
+  //freopen(".out", "w", stdout);
+
+  cout << fixed << setprecision(10);
+
+  int testcases = 1;
+  //cin >> testcases;
+  while (testcases--) {
+    solution();
+  }
+}
