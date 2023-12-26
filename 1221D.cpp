827c923e0cfaf7f9757904c9755a748ce136e27e
Naveen Kulkarni <naveenpkulkarni07@gmail.com>
2023-12-26 15:22:44 +0530

    Add dp problem.
    Make The Fence Great Again: https://codeforces.com/contest/1221/problem/D

---
+++ 1221D.cpp
@@ -0,0 +1,117 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef evenvalue
+  #include "debug.h"
+#else
+  #define debug(...)
+#endif
+
+using int64 = long long;
+using ld = long double;
+
+template<typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+template<typename T>
+using max_heap = priority_queue<T, vector<T>, less<T>>;
+
+namespace read {
+int Int() {
+  int x;
+  cin >> x;
+  return x;
+}
+int64 Int64() {
+  int64 x;
+  cin >> x;
+  return x;
+}
+char Char() {
+  char c;
+  cin >> c;
+  return c;
+}
+string String() {
+  string s;
+  cin >> s;
+  return s;
+}
+double Double() {
+  return stod(String());
+}
+ld LongDouble() {
+  return stold(String());
+}
+template<typename T1, typename T2>
+pair<T1, T2> Pair() {
+  pair<T1, T2> p;
+  cin >> p.first >> p.second;
+  return p;
+}
+template<typename T>
+vector<T> Vec(const int n) {
+  vector<T> v(n);
+  for (T &x : v) {
+    cin >> x;
+  }
+  return v;
+}
+template<typename T>
+vector<vector<T>> VecVec(const int n, const int m) {
+  vector<vector<T>> v(n);
+  for (vector<T> &vec : v) {
+    vec = Vec<T>(m);
+  }
+  return v;
+}
+}//namespace read
+
+constexpr int kInf = 1e9 + 10;
+constexpr int64 kInf64 = 1e17 + 10;
+constexpr int kMod = 1e9 + 7;
+constexpr int kMaxN = 2e5 + 10;
+
+inline void solution() {
+  const int n = read::Int();
+
+  vector<int> height(n);
+  vector<int> cost(n);
+
+  for (int i = 0; i < n; i++) {
+    height[i] = read::Int();
+    cost[i] = read::Int();
+  }
+
+  vector<vector<int64>> dp(n, vector<int64>(3, kInf64));
+
+  for (const int inc : {0, 1, 2}) {
+    dp[0][inc] = inc * cost[0];
+  }
+
+  for (int i = 1; i < n; i++) {
+    for (const int inc : {0, 1, 2}) {
+      for (const int prev_inc : {0, 1, 2}) {
+        if (height[i - 1] + prev_inc == height[i] + inc) continue;
+        dp[i][inc] = min(dp[i][inc], dp[i - 1][prev_inc] + inc * cost[i]);
+      }
+    }
+  }
+
+  cout << *min_element(dp[n - 1].begin(), dp[n - 1].end()) << '\n';
+}
+
+int32_t main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  //freopen(".in", "r", stdin);
+  //freopen(".out", "w", stdout);
+
+  cout << fixed << setprecision(10);
+
+  int testcases = 1;
+  cin >> testcases;
+  while (testcases--) {
+    solution();
+  }
+}
