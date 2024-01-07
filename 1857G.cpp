7d18f3ea7bc329fcee06b0f8066da0e19d2b8247
Naveen Kulkarni <naveenpkulkarni07@gmail.com>
2024-01-07 20:03:04 +0530

    Add dsu + combinatorics problem.
    Counting Graphs: https://codeforces.com/contest/1857/problem/G

---
+++ 1857G.cpp
@@ -0,0 +1,302 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef evenvalue
+  #include "debug.h"
+#else
+  #define debug(...)
+#endif
+
+using int64 = long long;
+using ld = long double;
+
+template<typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+template<typename T>
+using max_heap = priority_queue<T, vector<T>, less<T>>;
+
+namespace read {
+int Int() {
+  int x;
+  cin >> x;
+  return x;
+}
+int64 Int64() {
+  int64 x;
+  cin >> x;
+  return x;
+}
+char Char() {
+  char c;
+  cin >> c;
+  return c;
+}
+string String() {
+  string s;
+  cin >> s;
+  return s;
+}
+double Double() {
+  return stod(String());
+}
+ld LongDouble() {
+  return stold(String());
+}
+template<typename T1, typename T2>
+pair<T1, T2> Pair() {
+  pair<T1, T2> p;
+  cin >> p.first >> p.second;
+  return p;
+}
+template<typename T>
+vector<T> Vec(const int n) {
+  vector<T> v(n);
+  for (T &x : v) {
+    cin >> x;
+  }
+  return v;
+}
+template<typename T>
+vector<vector<T>> VecVec(const int n, const int m) {
+  vector<vector<T>> v(n);
+  for (vector<T> &vec : v) {
+    vec = Vec<T>(m);
+  }
+  return v;
+}
+}//namespace read
+
+constexpr int kInf = 1e9 + 10;
+constexpr int64 kInf64 = 1e15 + 10;
+constexpr int kMod = 998244353;
+constexpr int kMaxN = 2e5 + 10;
+
+template<unsigned int M>
+struct modint {
+  int64 x;
+
+  modint(const modint<M> &m) : x(m.x) {}
+  explicit modint(const int64 _x) : x((_x >= 0 ? _x % M : M + (_x % (int) M))) {}
+  explicit modint(const int _x) : x((_x >= 0 ? _x % M : M + (_x % (int) M))) {}
+  explicit modint() : x(0) {}
+
+  [[nodiscard]] modint<M> inv() const {
+    return power(M - 2);
+  }
+
+  [[nodiscard]] modint<M> power(int64 exp) const {
+    modint<M> res(1), base(x);
+    while (exp) {
+      if (exp & 1) res *= base;
+      base *= base;
+      exp >>= 1;
+    }
+    return res;
+  }
+
+  [[nodiscard]] modint<M> power(modint<M> &exp) const {
+    return power(exp.x);
+  }
+
+  modint<M> &operator=(const int _x) {
+    x = (_x >= 0 ? _x % M : M + (_x % M));
+    return *this;
+  }
+
+  modint<M> &operator=(const int64 _x) {
+    x = (_x >= 0 ? _x % M : M + (_x % M));
+    return *this;
+  }
+
+  modint<M> &operator=(const modint<M> &other) {
+    x = other.x;
+    return *this;
+  }
+
+  modint<M> &operator+=(const modint<M> &other) {
+    x += other.x;
+    x -= (x >= M ? M : 0);
+    return *this;
+  }
+
+  modint<M> &operator-=(const modint<M> &other) {
+    x += M - other.x;
+    x -= (x >= M ? M : 0);
+    return *this;
+  }
+
+  modint<M> &operator*=(const modint<M> &other) {
+    x *= other.x;
+    x %= M;
+    return *this;
+  }
+
+  modint<M> &operator/=(const modint<M> &other) {
+    x = (*this * other.inv()).x;
+    return *this;
+  }
+
+  friend modint<M> operator+(const modint<M> &lhs, const modint<M> &rhs) {
+    return (modint(lhs) += rhs);
+  }
+
+  friend modint<M> operator-(const modint<M> &lhs, const modint<M> &rhs) {
+    return (modint(lhs) -= rhs);
+  }
+
+  friend modint<M> operator*(const modint<M> &lhs, const modint<M> &rhs) {
+    return (modint(lhs) *= rhs);
+  }
+
+  friend modint<M> operator/(const modint<M> &lhs, const modint<M> &rhs) {
+    return (modint(lhs) /= modint(rhs));
+  }
+
+  friend bool operator==(const modint<M> &lhs, const modint<M> &rhs) {
+    return (lhs.x == rhs.x);
+  }
+
+  friend bool operator!=(const modint<M> &lhs, const modint<M> &rhs) {
+    return (lhs.x != rhs.x);
+  }
+
+  friend std::ostream &operator<<(std::ostream &os, const modint<M> &m) {
+    return (os << m.x);
+  }
+
+  friend std::istream &operator>>(std::istream &in, modint<M> &m) {
+    return (in >> m.x);
+  }
+
+  modint<M> &operator++() {
+    x++;
+    x = (x == M ? 0 : x);
+    return *this;
+  }
+
+  modint<M> &operator--() {
+    x = (x == 0 ? M : x);
+    x--;
+    return *this;
+  }
+
+  modint<M> operator++(int) {
+    modint result = *this;
+    ++*this;
+    return result;
+  }
+
+  modint<M> operator--(int) {
+    modint result = *this;
+    --*this;
+    return result;
+  }
+};
+
+class dsu {
+  int n;
+  std::vector<int> e;
+
+  int pfind(const int x) {
+    return (e[x] < 0 ? x : e[x] = pfind(e[x]));
+  }
+
+public:
+  dsu() : n(0), comp(0) {}
+  explicit dsu(const int n) : n(n), comp(n), e(n, -1) {}
+
+  int comp;
+
+  int find(const int x) {
+    assert(0 <= x and x < n);
+    return pfind(x);
+  }
+
+  bool unite(int x, int y) {
+    x = find(x), y = find(y);
+    if (x == y) return false;
+    if (e[x] > e[y]) swap(x, y);
+    e[x] += e[y];
+    e[y] = x;
+    comp--;
+    return true;
+  }
+
+  bool same(const int x, const int y) {
+    return (find(x) == find(y));
+  }
+
+  int size(const int x) {
+    return -e[find(x)];
+  }
+
+  std::vector<std::vector<int>> components() {
+    std::vector<std::vector<int>> res(n);
+    for (int x = 0; x < n; x++) {
+      res[pfind(x)].push_back(x);
+    }
+    res.erase(
+        std::remove_if(res.begin(), res.end(), [&](const std::vector<int> &v) { return v.empty(); }),
+        res.end());
+    return res;
+  }
+};
+
+struct edge {
+  int x;
+  int y;
+  int w;
+
+  bool operator<(const edge &other) const {
+    return w < other.w;
+  }
+};
+
+using mint = modint<kMod>;
+
+inline void solution() {
+  const int n = read::Int();
+  const int s = read::Int();
+
+  vector<edge> edges(n - 1);
+  for (auto &[x, y, w] : edges) {
+    x = read::Int() - 1;
+    y = read::Int() - 1;
+    w = read::Int();
+  }
+
+  sort(edges.begin(), edges.end());
+
+  dsu d(n);
+  mint ans(1);
+
+  for (const auto [x, y, w] : edges) {
+    const int ways = s - w + 1;
+
+    const int c1 = d.size(x);
+    const int c2 = d.size(y);
+
+    const int64 new_edges = c1 * 1LL * c2 - 1;
+    ans *= mint(ways).power(new_edges);
+
+    d.unite(x, y);
+  }
+
+  cout << ans << '\n';
+}
+
+int32_t main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  //freopen(".in", "r", stdin);
+  //freopen(".out", "w", stdout);
+
+  cout << fixed << setprecision(10);
+
+  int testcases = 1;
+  cin >> testcases;
+  while (testcases--) {
+    solution();
+  }
+}
