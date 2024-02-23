44973c085247984d1ecbb772d27a7cc9df3e63ce
Naveen Kulkarni <naveenpkulkarni07@gmail.com>
2024-02-23 16:09:03 +0530

    Add dp + "venice-technique" problem.
    Copy or Prefix Sum: https://codeforces.com/contest/1485/problem/F

---
+++ 1485F.cpp
@@ -0,0 +1,232 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef evenvalue
+  #include "debug.h"
+#else
+  #define debug(...)
+#endif
+
+using int64 = long long;
+using ld = long double;
+
+template<typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+template<typename T>
+using max_heap = priority_queue<T, vector<T>, less<T>>;
+
+namespace Read {
+int Int() {
+  int x;
+  cin >> x;
+  return x;
+}
+int64 Int64() {
+  int64 x;
+  cin >> x;
+  return x;
+}
+char Char() {
+  char c;
+  cin >> c;
+  return c;
+}
+string String() {
+  string s;
+  cin >> s;
+  return s;
+}
+double Double() {
+  return stod(String());
+}
+ld LongDouble() {
+  return stold(String());
+}
+template<typename T1, typename T2>
+pair<T1, T2> Pair() {
+  pair<T1, T2> p;
+  cin >> p.first >> p.second;
+  return p;
+}
+template<typename T>
+vector<T> Vec(const int n) {
+  vector<T> v(n);
+  for (T &x : v) {
+    cin >> x;
+  }
+  return v;
+}
+template<typename T>
+vector<vector<T>> VecVec(const int n, const int m) {
+  vector<vector<T>> v(n);
+  for (vector<T> &vec : v) {
+    vec = Vec<T>(m);
+  }
+  return v;
+}
+}//namespace Read
+
+constexpr int kInf = 1e9 + 10;
+constexpr int64 kInf64 = 1e15 + 10;
+constexpr int kMod = 1e9 + 7;
+constexpr int kMaxN = 2e5 + 10;
+
+template<unsigned int M>
+struct modint {
+  int64 x;
+
+  modint(const modint<M> &m) : x(m.x) {}
+  explicit modint(const int64 _x) : x((_x >= 0 ? _x % M : M + (_x % (int) M))) {}
+  explicit modint(const int _x) : x((_x >= 0 ? _x % M : M + (_x % (int) M))) {}
+  explicit modint() : x(0) {}
+
+  [[nodiscard]] modint<M> inv() const {
+    return power(M - 2);
+  }
+
+  [[nodiscard]] modint<M> power(int64 exp) const {
+    modint<M> res(1), base(x);
+    while (exp) {
+      if (exp & 1) res *= base;
+      base *= base;
+      exp >>= 1;
+    }
+    return res;
+  }
+
+  [[nodiscard]] modint<M> power(modint<M> &exp) const {
+    return power(exp.x);
+  }
+
+  modint<M> &operator=(const int _x) {
+    x = (_x >= 0 ? _x % M : M + (_x % M));
+    return *this;
+  }
+
+  modint<M> &operator=(const int64 _x) {
+    x = (_x >= 0 ? _x % M : M + (_x % M));
+    return *this;
+  }
+
+  modint<M> &operator=(const modint<M> &other) {
+    x = other.x;
+    return *this;
+  }
+
+  modint<M> &operator+=(const modint<M> &other) {
+    x += other.x;
+    x -= (x >= M ? M : 0);
+    return *this;
+  }
+
+  modint<M> &operator-=(const modint<M> &other) {
+    x += M - other.x;
+    x -= (x >= M ? M : 0);
+    return *this;
+  }
+
+  modint<M> &operator*=(const modint<M> &other) {
+    x *= other.x;
+    x %= M;
+    return *this;
+  }
+
+  modint<M> &operator/=(const modint<M> &other) {
+    x = (*this * other.inv()).x;
+    return *this;
+  }
+
+  friend modint<M> operator+(const modint<M> &lhs, const modint<M> &rhs) {
+    return (modint(lhs) += rhs);
+  }
+
+  friend modint<M> operator-(const modint<M> &lhs, const modint<M> &rhs) {
+    return (modint(lhs) -= rhs);
+  }
+
+  friend modint<M> operator*(const modint<M> &lhs, const modint<M> &rhs) {
+    return (modint(lhs) *= rhs);
+  }
+
+  friend modint<M> operator/(const modint<M> &lhs, const modint<M> &rhs) {
+    return (modint(lhs) /= modint(rhs));
+  }
+
+  friend bool operator==(const modint<M> &lhs, const modint<M> &rhs) {
+    return (lhs.x == rhs.x);
+  }
+
+  friend bool operator!=(const modint<M> &lhs, const modint<M> &rhs) {
+    return (lhs.x != rhs.x);
+  }
+
+  friend std::ostream &operator<<(std::ostream &os, const modint<M> &m) {
+    return (os << m.x);
+  }
+
+  friend std::istream &operator>>(std::istream &in, modint<M> &m) {
+    return (in >> m.x);
+  }
+
+  modint<M> &operator++() {
+    x++;
+    x = (x == M ? 0 : x);
+    return *this;
+  }
+
+  modint<M> &operator--() {
+    x = (x == 0 ? M : x);
+    x--;
+    return *this;
+  }
+
+  modint<M> operator++(int) {
+    modint result = *this;
+    ++*this;
+    return result;
+  }
+
+  modint<M> operator--(int) {
+    modint result = *this;
+    --*this;
+    return result;
+  }
+};
+
+using mint = modint<kMod>;
+
+inline void solution() {
+  const int n = Read::Int();
+  const vector<int> b = Read::Vec<int>(n);
+
+  map<int64, mint> dp;
+
+  mint total_ways(1);
+  dp[0] = 1;
+
+  int64 zero_level = 0;
+  for (const int x : b) {
+    zero_level -= x;
+    const mint diff = total_ways - dp[zero_level + x];
+    dp[zero_level + x] = total_ways;
+    total_ways += diff;
+  }
+
+  cout << total_ways << '\n';
+}
+
+int32_t main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  //freopen(".in", "r", stdin);
+  //freopen(".out", "w", stdout);
+
+  cout << fixed << setprecision(10);
+
+  int testcases = 1;
+  cin >> testcases;
+  while (testcases--) {
+    solution();
+  }
+}
