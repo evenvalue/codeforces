e094455a8bdaed6255703f84bc015afb62111ade
Naveen Kulkarni <naveenpkulkarni07@gmail.com>
2024-03-17 02:53:17 +0530

    Add interactive problem.

---
+++ 1934D2.cpp
@@ -0,0 +1,148 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+#ifdef evenvalue
+  #include "debug.h"
+#else
+  #define debug(...)
+#endif
+
+using int64 = long long;
+using ld = long double;
+
+template<typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+template<typename T>
+using max_heap = priority_queue<T, vector<T>, less<T>>;
+
+namespace Read {
+int Int() {
+  int x;
+  cin >> x;
+  return x;
+}
+int64 Int64() {
+  int64 x;
+  cin >> x;
+  return x;
+}
+char Char() {
+  char c;
+  cin >> c;
+  return c;
+}
+string String() {
+  string s;
+  cin >> s;
+  return s;
+}
+double Double() {
+  return stod(String());
+}
+ld LongDouble() {
+  return stold(String());
+}
+template<typename T1, typename T2>
+pair<T1, T2> Pair() {
+  pair<T1, T2> p;
+  cin >> p.first >> p.second;
+  return p;
+}
+template<typename T>
+vector<T> Vec(const int n) {
+  vector<T> v(n);
+  for (T &x : v) {
+    cin >> x;
+  }
+  return v;
+}
+template<typename T>
+vector<vector<T>> VecVec(const int n, const int m) {
+  vector<vector<T>> v(n);
+  for (vector<T> &vec : v) {
+    vec = Vec<T>(m);
+  }
+  return v;
+}
+}//namespace Read
+
+constexpr int kInf = 1e9 + 10;
+constexpr int64 kInf64 = 1e15 + 10;
+constexpr int kMod = 1e9 + 7;
+constexpr int kMaxN = 2e5 + 10;
+
+using uint = unsigned int;
+
+int popcount(int x) {
+  return popcount(uint(x));
+}
+
+pair<int, int> ask() {
+  const int x = Read::Int();
+  const int y = Read::Int();
+  return {x, y};
+}
+
+void say(const pair<int, int> p) {
+  cout << p.first << ' ' << p.second << endl;
+}
+
+bool return_type(const int x, const int y) {
+  if (x == 0 and y == 0) return true;
+  if (x == -1 and y == -1) return true;
+  return false;
+}
+
+int choose(const int x, const int y) {
+  if (popcount(x) % 2 == 0) return x;
+  if (popcount(y) % 2 == 0) return y;
+  assert(false);
+}
+
+pair<int, int> split(const int x) {
+  int p1 = 1;
+  while (2 * p1 < x) {
+    p1 <<= 1;
+  }
+  return {p1, x ^ p1};
+}
+
+inline void solution() {
+  int n = Read::Int();
+  if (popcount(n) % 2 == 1) {
+    cout << "second" << endl;
+    const auto [x, y] = ask();
+    if (return_type(x, y)) goto END;
+    n = choose(x, y);
+  } else {
+    cout << "first" << endl;
+  }
+
+  while (true) {
+    say(split(n));
+    const auto [x, y] = ask();
+    if (return_type(x, y)) goto END;
+    n = choose(x, y);
+  }
+
+END:
+  return;
+}
+
+int32_t main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  //freopen(".in", "r", stdin);
+  //freopen(".out", "w", stdout);
+
+  cout << fixed << setprecision(10);
+
+  int testcases = 1;
+  cin >> testcases;
+  while (testcases--) {
+    solution();
+  }
+}
