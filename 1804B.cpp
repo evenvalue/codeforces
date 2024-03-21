5f2d4bf2158cb1be836fd72c92e67a213d502120
Naveen Kulkarni <naveenpkulkarni07@gmail.com>
2024-03-21 15:02:12 +0530

    Add solutions to Nebius Welcome Round (Div. 1 + Div. 2)
    Dashboard: https://codeforces.com/contest/1804

---
+++ 1804B.cpp
@@ -0,0 +1,133 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef evenvalue
+  #include "debug.h"
+#else
+  #define debug(...)
+#endif
+
+using int64 = long long;
+using ld = long double;
+
+template<typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+template<typename T>
+using max_heap = priority_queue<T, vector<T>, less<T>>;
+
+namespace read {
+int Int() {
+  int x;
+  cin >> x;
+  return x;
+}
+int64 Int64() {
+  int64 x;
+  cin >> x;
+  return x;
+}
+char Char() {
+  char c;
+  cin >> c;
+  return c;
+}
+string String() {
+  string s;
+  cin >> s;
+  return s;
+}
+double Double() {
+  return stod(String());
+}
+ld LongDouble() {
+  return stold(String());
+}
+template<typename T1, typename T2>
+pair<T1, T2> Pair() {
+  pair<T1, T2> p;
+  cin >> p.first >> p.second;
+  return p;
+}
+template<typename T>
+vector<T> Vec(const int n) {
+  vector<T> v(n);
+  for (T &x : v) {
+    cin >> x;
+  }
+  return v;
+}
+template<typename T>
+vector<vector<T>> VecVec(const int n, const int m) {
+  vector<vector<T>> v(n);
+  for (vector<T> &vec : v) {
+    vec = Vec<T>(m);
+  }
+  return v;
+}
+}//namespace read
+
+constexpr int kInf = 1e9 + 10;
+constexpr int64 kInf64 = 1e15 + 10;
+constexpr int kMod = 1e9 + 7;
+constexpr int kMaxN = 2e5 + 10;
+
+struct range {
+  int l;
+  int r;
+};
+
+bool single_intersect(const range &r1, const range &r2) {
+  if (r1.l <= r2.l and r2.l <= r1.r) return true;
+  if (r1.l <= r2.r and r2.r <= r1.r) return true;
+  return false;
+}
+
+bool intersect(const range &r1, const range &r2) {
+  return single_intersect(r1, r2) or single_intersect(r2, r1);
+}
+
+inline void solution() {
+  const int n = read::Int();
+  const int k = read::Int();
+  const int d = read::Int();
+  const int w = read::Int();
+
+  vector<range> people(n);
+  for (int i = 0; i < n; i++) {
+    people[i].l = read::Int();
+    people[i].r = people[i].l + w;
+  }
+
+  int ans = 0;
+  for (int i = 0; i < n;) {
+    ans++;
+    const int start = people[i].r;
+    const int end = start + d;
+    const range cur = {start, end};
+    for (int j = 0; j < k; j++) {
+      if (i < n and intersect(people[i], cur)) {
+        i++;
+      } else {
+        break;
+      }
+    }
+  }
+
+  cout << ans << '\n';
+}
+
+int32_t main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  //freopen(".in", "r", stdin);
+  //freopen(".out", "w", stdout);
+
+  cout << fixed << setprecision(10);
+
+  int testcases = 1;
+  cin >> testcases;
+  while (testcases--) {
+    solution();
+  }
+}
