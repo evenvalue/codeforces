0d4f4bc65855a60939ac30a1459e115faa77bb9b
Naveen Kulkarni <naveenpkulkarni07@gmail.com>
2024-10-07 11:20:52 +0530

    Add code. :P

---
+++ 1238D.cpp
@@ -0,0 +1,97 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef LOCAL
+  #define debug(...)
+#endif
+
+using int64 = long long;
+using ld = long double;
+
+template<typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+template<typename T>
+using max_heap = priority_queue<T, vector<T>, less<T>>;
+
+template<typename T>
+T read() {
+  T x;
+  cin >> x;
+  return x;
+}
+
+template<typename T1, typename T2>
+pair<T1, T2> read_pair() {
+  pair<T1, T2> p;
+  cin >> p.first;
+  cin >> p.second;
+  return p;
+}
+
+template<typename T>
+vector<T> read_vec(const int n, const int index = 0) {
+  vector<T> v(n + index);
+  for (int i = index; i < n + index; i++) {
+    v[i] = read<T>();
+  }
+  return v;
+}
+
+template<typename T>
+vector<vector<T>> read_vec_vec(const int n, const int m, const int index_n = 0, const int index_m = 0) {
+  vector<vector<T>> grid(n + index_n);
+  for (int i = index_n; i < n + index_n; i++) {
+    grid[i] = read_vec<T>(m, index_m);
+  }
+  return grid;
+}
+
+constexpr int kInf = 1e9 + 10;
+constexpr int64 kInf64 = 1e15 + 10;
+constexpr int kMod = 1e9 + 7;
+constexpr int kMaxN = 2e5 + 10;
+
+inline void solution() {
+  const int n = read<int>();
+  auto s = read<string>();
+
+  int64 ans = 0;
+
+  vector<int> pref(n, 1);
+  for (int i = 1; i < n; i++) {
+    if (s[i] != s[i - 1]) {
+      ans += pref[i - 1];
+    } else {
+      pref[i] += pref[i - 1];
+    }
+  }
+
+  vector<int> suff(n, 1);
+  for (int i = n - 2; i >= 0; i--) {
+    if (s[i] != s[i + 1]) {
+      ans += suff[i + 1] - 1;
+    } else {
+      suff[i] += suff[i + 1];
+    }
+  }
+
+  ans = (n * 1LL * (n - 1) / 2) - ans;
+
+  cout << ans << '\n';
+}
+
+int32_t main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  //freopen(".in", "r", stdin);
+  //freopen(".out", "w", stdout);
+
+  cout << fixed << setprecision(10);
+
+  int testcases = 1;
+  //cin >> testcases;
+  while (testcases--) {
+    solution();
+  }
+}
